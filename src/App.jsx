e7969cd4ffe282315a33ca1a5eddb6cacdfb10fc
Dev2trippldee <Dev2trippldee@users.noreply.github.com>
2026-01-20 11:36:29 +0530

    initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">
+        React + Tailwind is Ready 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
